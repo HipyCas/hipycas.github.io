a6ded9db0d210a3c932b8625cff9ce71db50e138
Hipy Cas <hipycas@gmail.com>
2021-04-30 19:58:27 +0200

    Create snowpack config file and add extensions

---
+++ snowpack.config.js
@@ -0,0 +1,27 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  root: './src/',
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    [
+      'snowpack-plugin-raw-file-loader',
+      {
+        exts: ['.md', '.html'], // Add file extensions saying what files should be loaded as strings in your snowpack application. Default: '.txt'
+      },
+    ],
+  ], // ! '@snowpack/plugin-typescript'], NOT WORKING
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
